2973fd29675a1ae1c6a560955f0c7a158229077c
Raphael Bühlmann <raphael.buehlmann@hotmail.com>
2021-04-15 15:43:08 +0200

    Added Unit Test for Command Handler

--- Backend/QuizPrototype.WebApi/QuizPrototype.Testing/Commands/SendFrageCommandHandlerTest.cs
+++ Backend/QuizPrototype.WebApi/QuizPrototype.Testing/Commands/SendFrageCommandHandlerTest.cs
@@ -1,18 +1,48 @@
 ﻿using NUnit.Framework;
+using QuizPrototype.Domain.Interfaces;
+using QuizPrototype.WebApi.Commands;
+using Moq;
+using QuizPrototype.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace QuizPrototype.Testing.Commands
 {
     class SendFrageCommandHandlerTest
     {
+        private Mock<IFrageRepository> frageRepository;
+
         [SetUp]
         public void Setup()
         {
+            frageRepository = new Mock<IFrageRepository>();
+            frageRepository.Setup(x => x.GetCurrentFrage()).
+                Returns(Task.FromResult(new Frage
+                {
+                    Id = 1,
+                    FrageText = "Ordnen nach Grösse",
+                    Auswahlmoeglichkeiten = new List<Auswahl>
+                    {
+                        new Auswahl { Id = 1, AuswahlText = "Test", Order=2, FrageId = 1 },
+                        new Auswahl { Id = 2, AuswahlText = "Test2", Order=1, FrageId = 1 }
+                    }
+                }));
         }
 
         [Test]
-        public void Test1()
+        public async Task TestGetCurrentFrageFromHandlerAsync()
         {
-            Assert.Pass();
+            // Arrange
+            var sut = new SendFrageCommandHandler(frageRepository.Object);
+            var command = new SendFrageCommand();
+            var expectedFrageId = 1;
+
+            // Act
+            var result = await sut.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(expectedFrageId, result.Id);
         }
     }
 }
